f850cfb9fbf92cc9aa5d492dc8686b110f579857
Joao Lopes <jocolopes@gmail.com>
2024-11-15 23:31:15 +0000

    Fix typo

--- src/server/middlewares/protectCoordinatesApi.ts
+++ src/server/middlewares/protectCoordinatesApi.ts
@@ -4,10 +4,11 @@ import { env } from "~/env";
 export default function protectCoordinatesApi(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
 
-if (request.url.includes("/api/coordinates") && request.method !== "GET") {
-  const apiPassword = requestHeaders.get("x-api-password");
+  if (request.url.includes("/api/coordinates") && request.method !== "GET") {
+    const apiPassword = requestHeaders.get("x-api-password");
 
-  if (apiPassword !== env.API_PASSWORD) {
-    return new Response("Unauthorized", { status: 401 });
+    if (apiPassword !== env.API_PASSWORD) {
+      return new Response("Unauthorized", { status: 401 });
+    }
   }
 }
